eda17b8862e0c854f0e89cd162c42c45a5267fbe
Benjamin Glass <bisaacg@gmail.com>
2022-08-25 17:04:23 -0400

    Added snapItemToContainer

--- core/containers/Container.js
+++ core/containers/Container.js
@@ -15,25 +15,18 @@ export default class Container extends Item {
         //if (this.constructor == Container)
         //    throw new Error("Abstract classes can't instantiated.");
 
-        this.validItems += validItems;
+        this.#validItems = validItems;
     }
 
-    checkValid(item) {
-        if (this.validItems.includes(item) &&
-        (item.x+this.width >= this.x && item.x-this.width <= this.x) &&
+    snapItemToContainer(item) {
+        // Item is within container and not selected
+        if ((item.x+this.width >= this.x && item.x-this.width <= this.x) &&
         (item.y+this.height >= this.y && item.y-this.height <= this.y)
-        && this.activeItem == null
         && item.selected == false) {
             item.x = this.x;
             item.y = this.y;
-            this.activeItem = item;
-            console.log('one');
+            this.#activeItem = item;
             return true;
-        }
-        else {
-            //this.activeItem = null;
-            console.log('two');
-            return false;
         }
     }
 
@@ -44,5 +37,17 @@ export default class Container extends Item {
         ctx.strokeStyle = "black";
         ctx.rect(this.x,this.y,this.width,this.height);
         ctx.stroke();
+
+        // There is currently no item in the container
+        if (this.#activeItem == null)
+            // check for valid items
+            this.#validItems.forEach((item) => {
+                this.snapItemToContainer(item);
+            });
+
+        // If active item is selected release it from
+        // the container
+        else if (this.#activeItem.selected)
+            this.#activeItem = null;
     }
 }
